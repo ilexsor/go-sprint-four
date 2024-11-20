443f482fc08f30af8d91644235e77076681e9726
ilexsor <ilexsor@gmail.com>
2024-11-20 18:38:13 +0400

    Add comments and cmInM variable

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-Ура! Вы добрались до финального проекта спринта. Это ваш первый проект, который вы отправите на проверку через Git. Прежде чем приступить к выполнению задачи, изучите технические подробности. 
+## ftracker package
+### Uses for calculate human activity for
+- Runing
+- Swiming
+- Walking
+
+[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://github.com/ilexsor/go-sprint-four)
+
+## Run tests
+```sh
+go test -v
+```
--- ftracker.go
+++ ftracker.go
@@ -105,7 +105,7 @@ const (
 // height float64 — рост пользователя.
 func WalkingSpentCalories(action int, duration, weight, height float64) float64 {
 	meanWalkingSpeed := meanSpeed(action, duration) * kmhInMsec
-	return (walkingCaloriesWeightMultiplier*weight + (math.Pow(meanWalkingSpeed, 2)/(height/100))*walkingSpeedHeightMultiplier*weight) * duration * minInH
+	return (walkingCaloriesWeightMultiplier*weight + (math.Pow(meanWalkingSpeed, 2)/(height/cmInM))*walkingSpeedHeightMultiplier*weight) * duration * minInH
 }
 
 // Константы для расчета калорий, расходуемых при плавании.
